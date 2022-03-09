6e98618914e179a70d2f5aedc903a79f740f4df0
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2022-03-09 14:34:24 +0800

    try

--- src/state/user/hooks/index.tsx
+++ src/state/user/hooks/index.tsx
@@ -417,7 +417,7 @@ export function toV2LiquidityToken([tokenA, tokenB]: [Token, Token]): Token {
  */
 export function useTrackedTokenPairs(): [Token, Token][] {
   const { chainId } = useActiveWeb3React()
-  const tokens = useAllTokens(true)
+  const tokens = useAllTokens()
 
   // pinned pairs
   const pinnedPairs = useMemo(() => (chainId ? PINNED_PAIRS[chainId] ?? [] : []), [chainId])

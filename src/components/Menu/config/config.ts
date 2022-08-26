f6e2acf8095ed1ec92219883b4c3728a808b1ba7
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2022-08-26 18:33:19 +0800

    fix

--- src/components/Menu/config/config.ts
+++ src/components/Menu/config/config.ts
@@ -80,6 +80,7 @@ const config: (t: ContextApi['t'], isDark: boolean, languageCode?: string, chain
         icon: EarnIcon,
         fillIcon: EarnFillIcon,
         image: '/images/decorations/pe2.png',
+        supportChainIds: SUPPORT_ONLY_BSC,
         items: [
           {
             label: t('Farms'),
@@ -88,7 +89,6 @@ const config: (t: ContextApi['t'], isDark: boolean, languageCode?: string, chain
           {
             label: t('Pools'),
             href: '/pools',
-            supportChainIds: SUPPORT_ONLY_BSC,
           },
         ].map((item) => addMenuItemSupported(item, chainId)),
       },

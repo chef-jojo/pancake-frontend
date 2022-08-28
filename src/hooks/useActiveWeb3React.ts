166726ce44aff07a13aa339d31fb87ca3d37e170
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2022-08-28 23:32:18 +0800

    use atom query

--- src/hooks/useActiveWeb3React.ts
+++ src/hooks/useActiveWeb3React.ts
@@ -1,26 +1,27 @@
 import { useWeb3React } from '@pancakeswap/wagmi'
 import replaceBrowserHistory from 'utils/replaceBrowserHistory'
 import { useRouter } from 'next/router'
+import { useAtomValue } from 'jotai'
 import { useEffect } from 'react'
 import { isChainSupported } from 'utils/wagmi'
 import { useProvider } from 'wagmi'
 import { ChainId } from '@pancakeswap/sdk'
-import { useActiveChainId } from './useActiveChainId'
+import { queryChainIdAtom, useActiveChainId } from './useActiveChainId'
 import { useSwitchNetworkLoading } from './useSwitchNetworkLoading'
 
 export function useNetworkConnectorUpdater() {
   const { chainId, isConnecting } = useActiveWeb3React()
   const [loading] = useSwitchNetworkLoading()
-  const router = useRouter()
+  const queryChainId = useAtomValue(queryChainIdAtom)
 
   useEffect(() => {
     if (loading || isConnecting) return
-    const parsedQueryChainId = Number(router.query.chainId)
+    const parsedQueryChainId = Number(queryChainId)
     if (!parsedQueryChainId && chainId === ChainId.BSC) return
     if (parsedQueryChainId !== chainId && isChainSupported(chainId)) {
       replaceBrowserHistory('chainId', chainId === ChainId.BSC ? null : chainId)
     }
-  }, [chainId, isConnecting, loading, router.query.chainId])
+  }, [chainId, isConnecting, loading, queryChainId])
 }
 
 /**
